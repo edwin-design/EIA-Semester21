a495ad4ffdd7a76f38db33324afbedb377303a74
Edwin Skrijelj <edwin.skrijelj@hs-furtwangen.de>
2021-05-11 15:47:18 +0200

    Flowchart

--- aufgabe7.ts
+++ aufgabe7.ts
@@ -2,33 +2,33 @@ const Sound:HTMLAudioElement []= [new Audio ('Aufgabe7/A.mp3'), new Audio ('Aufg
     
 
 
-window.addEventListener("load", function playSample(){
-    document.querySelector("#first").addEventListener("click", function(){ (Sound[0]).play() } )
-    document.querySelector("#second").addEventListener("click", function(){ (Sound[1]).play() } )
-    document.querySelector("#third").addEventListener("click", function(){ (Sound[2]).play();} )
-    document.querySelector("#fourth").addEventListener("click", function(){ (Sound[3]).play();} )
-    document.querySelector("#fifth").addEventListener("click", function(){ (Sound[4]).play();} )
-    document.querySelector("#sixth").addEventListener("click", function(){ (Sound[5]).play();} )
-    document.querySelector("#seventh").addEventListener("click", function(){ (Sound[6]).play();} )
-    document.querySelector("#eighth").addEventListener("click", function(){ (Sound[7]).play();} )
-    document.querySelector("#ninth").addEventListener("click", function(){ (Sound[8]).play();} )
-    document.querySelector("#play").addEventListener("click", function playBeat(){
+window.addEventListener("load", function playSample(): void{
+    document.querySelector("#first").addEventListener("click", function(): void{ (Sound[0]).play() } )
+    document.querySelector("#second").addEventListener("click", function(): void{ (Sound[1]).play() } )
+    document.querySelector("#third").addEventListener("click", function(): void{ (Sound[2]).play();} )
+    document.querySelector("#fourth").addEventListener("click", function(): void{ (Sound[3]).play();} )
+    document.querySelector("#fifth").addEventListener("click", function(): void{ (Sound[4]).play();} )
+    document.querySelector("#sixth").addEventListener("click", function(): void{ (Sound[5]).play();} )
+    document.querySelector("#seventh").addEventListener("click", function(): void{ (Sound[6]).play();} )
+    document.querySelector("#eighth").addEventListener("click", function(): void{ (Sound[7]).play();} )
+    document.querySelector("#ninth").addEventListener("click", function(): void{ (Sound[8]).play();} )
+    document.querySelector("#play").addEventListener("click", function playBeat(): void{
 
-  setInterval(function(){
+  setInterval(function(): void{
 
     Sound[3].play() }, 300);
 
-  setInterval(function(){
+  setInterval(function(): void{
 
     (Sound[2]).play()}, 700)
 
-    setInterval(function(){
+    setInterval(function(): void{
 
         (Sound[4]).play()}, 2400)
     
       
 
-  setInterval(function(){
+  setInterval(function(): void{
 
      (Sound[0]).play()}, 1000)
 
